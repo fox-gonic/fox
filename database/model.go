8206e6d75246a26160d1213486b56f5be6c6848a
Miclle Zheng <miclle.zheng@gmail.com>
2023-02-01 01:20:07 +0800

    Omit empty database model DeleteAt field

--- database/model.go
+++ database/model.go
@@ -12,10 +12,10 @@ import (
 //	  database.Model
 //	}
 type Model struct {
-	ID        int64                 `json:"id"         gorm:"primarykey"`
+	ID        int64                 `json:"id"                   gorm:"primarykey"`
 	CreatedAt int64                 `json:"created_at"`
 	UpdatedAt int64                 `json:"updated_at"`
-	DeletedAt soft_delete.DeletedAt `json:"deleted_at" gorm:"index"`
+	DeletedAt soft_delete.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
 const (
