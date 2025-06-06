55df86a8587242992d54078647e310fc85d896fa
jimyag <git@jimyag.com>
2025-06-06 16:17:15 +0800

    refactor(logger)!: change Level type from int8 to string (#12)
    BREAKING CHANGE: Level type has been changed from int8 to string.
    This change affects all code that uses the Level type directly.
    
    - Convert Level type from int8 to string for better readability
    - Add ZerologLevel() method to map string levels to zerolog.Level
    - Update logger implementation to use new Level type
    - Improve type safety and maintainability

--- logger/level.go
+++ logger/level.go
@@ -1,28 +1,56 @@
 package logger
 
+import "github.com/rs/zerolog"
+
 // Level type
-type Level int8
+type Level string
 
 const (
 	// DebugLevel defines debug log level.
-	DebugLevel Level = iota
+	DebugLevel Level = "debug"
 	// InfoLevel defines info log level.
-	InfoLevel
+	InfoLevel Level = "info"
 	// WarnLevel defines warn log level.
-	WarnLevel
+	WarnLevel Level = "warn"
 	// ErrorLevel defines error log level.
-	ErrorLevel
+	ErrorLevel Level = "error"
 	// FatalLevel defines fatal log level.
-	FatalLevel
+	FatalLevel Level = "fatal"
 	// PanicLevel defines panic log level.
-	PanicLevel
+	PanicLevel Level = "panic"
 	// NoLevel defines an absent log level.
-	NoLevel
+	NoLevel Level = "no"
 	// Disabled disables the logger.
-	Disabled
+	Disabled Level = "disabled"
 	// TraceLevel defines trace log level.
-	TraceLevel Level = -1
+	TraceLevel Level = "trace"
 )
+
+func (l Level) ZerologLevel() zerolog.Level {
+	switch l {
+	case DebugLevel:
+		return zerolog.DebugLevel
+	case InfoLevel:
+		return zerolog.InfoLevel
+	case WarnLevel:
+		return zerolog.WarnLevel
+	case ErrorLevel:
+		return zerolog.ErrorLevel
+	case FatalLevel:
+		return zerolog.FatalLevel
+	case PanicLevel:
+		return zerolog.PanicLevel
+	case NoLevel:
+		return zerolog.NoLevel
+	case Disabled:
+		return zerolog.Disabled
+	case TraceLevel:
+		return zerolog.TraceLevel
+	default:
+		// default to trace level
+		return zerolog.TraceLevel
+	}
+}
 
 var (
 	// DefaultLogLevel log level
